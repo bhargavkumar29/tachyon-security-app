c1ea076634ce9f7f68b1d8c8181ab638e956b62b
bhargav <5211421052@gvpcdpgc.edu.in>
2024-10-24 17:31:48 +0530

    Updated backend and frontend components, excluding index.css

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 import random
 import re
-
 app = Flask(__name__)
 
 # Enable CORS for the /add_device endpoint only from http://localhost:5173
